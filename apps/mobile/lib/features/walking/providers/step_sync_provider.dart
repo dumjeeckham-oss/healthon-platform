cfc164141fa3df69b24926521b1b3873505b769c
dumjeeckham-oss <dumjeeckham@gmail.com>
2026-07-08 15:00:48 +0900

    Add step sync provider with dependencies

---
+++ apps/mobile/lib/features/walking/providers/step_sync_provider.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../data/health_repository.dart';
+import '../data/step_repository_impl.dart';
+import '../services/step_sync_service.dart';
+
+final healthRepositoryProvider =
+    Provider<HealthRepository>(
+  (ref) => HealthRepository(),
+);
+
+final stepRepositoryProvider =
+    Provider<StepRepositoryImpl>(
+  (ref) => StepRepositoryImpl(),
+);
+
+final stepSyncServiceProvider =
+    Provider<StepSyncService>(
+  (ref) => StepSyncService(
+    healthRepository:
+        ref.read(healthRepositoryProvider),
+    stepRepository:
+        ref.read(stepRepositoryProvider),
+  ),
+);
