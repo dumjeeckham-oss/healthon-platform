2731618ef700186ba766a559c4446d6b00a8635c
dumjeeckham-oss <dumjeeckham@gmail.com>
2026-07-14 17:33:53 +0900

    Fix import path for challenge_provider

--- apps/mobile/lib/features/challenge/presentation/widgets/challenge_progress_section.dart
+++ apps/mobile/lib/features/challenge/presentation/widgets/challenge_progress_section.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../provider/challenge_provider.dart';
+import '../providers/challenge_provider.dart';
 import '../../domain/models/challenge_summary.dart';
 
 class ChallengeProgressSection extends ConsumerWidget {
