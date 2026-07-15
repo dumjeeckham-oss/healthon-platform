56628f2cb24064d8896ca47bb6c0a123edb72fda
dumjeeckham-oss <dumjeeckham@gmail.com>
2026-07-15 15:57:18 +0900

    Add walking data providers for steps, distance, and calories

---
+++ apps/mobile/lib/features/walking/presentation/providers/walking_provider.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../health/presentation/providers/health_provider.dart';
+
+final todayStepsProvider =
+    FutureProvider.autoDispose<int>((ref) async {
+  final repository = ref.read(healthRepositoryProvider);
+  return repository.getTodaySteps();
+});
+
+final distanceProvider =
+    FutureProvider.autoDispose<double>((ref) async {
+  final repository = ref.read(healthRepositoryProvider);
+  return repository.estimateDistanceKm();
+});
+
+final caloriesProvider =
+    FutureProvider.autoDispose<double>((ref) async {
+  final repository = ref.read(healthRepositoryProvider);
+  return repository.estimateCalories();
+});
+
+final weeklyStepsProvider =
+    FutureProvider.autoDispose<List<int>>((ref) async {
+  final repository = ref.read(healthRepositoryProvider);
+  return repository.getLast7DaysSteps();
+});
+
+final weeklyTotalProvider =
+    FutureProvider.autoDispose<int>((ref) async {
+  final repository = ref.read(healthRepositoryProvider);
+  return repository.getWeeklySteps();
+});
+
+final monthlyTotalProvider =
+    FutureProvider.autoDispose<int>((ref) async {
+  final repository = ref.read(healthRepositoryProvider);
+  return repository.getMonthlySteps();
+});
