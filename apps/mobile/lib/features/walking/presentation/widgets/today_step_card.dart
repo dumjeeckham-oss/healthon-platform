db013505a090230ab51ef0504c6dc33bc025b7b5
dumjeeckham-oss <dumjeeckham@gmail.com>
2026-07-15 15:47:26 +0900

    Import walking_provider in TodayStepCard

--- apps/mobile/lib/features/walking/presentation/widgets/today_step_card.dart
+++ apps/mobile/lib/features/walking/presentation/widgets/today_step_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../providers/walking_provider.dart';
 
 import '../providers/today_steps_provider.dart';
 
