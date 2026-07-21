1520da51d1ccb814ff4d4945025ba92426b64344
dumjeeckham-oss <dumjeeckham@gmail.com>
2026-07-21 18:01:34 +0900

    폴더 생성

---
+++ apps/mobile/lib/domain/dashboard_summary.dart
@@ -0,0 +1,31 @@
+class DashboardSummary {
+  final int todaySteps;
+
+  final double todayDistance;
+
+  final double calories;
+
+  final double challengeProgress;
+
+  final double totalKm;
+
+  final int familyRank;
+
+  final int streak;
+
+  final int forestLevel;
+
+  final String aiMessage;
+
+  const DashboardSummary({
+    required this.todaySteps,
+    required this.todayDistance,
+    required this.calories,
+    required this.challengeProgress,
+    required this.totalKm,
+    required this.familyRank,
+    required this.streak,
+    required this.forestLevel,
+    required this.aiMessage,
+  });
+}
