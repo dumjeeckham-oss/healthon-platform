668c4e2838b193c6e124aa84ff53b4aa3a7b73d9
dumjeeckham-oss <dumjeeckham@gmail.com>
2026-07-22 11:56:17 +0900

    Add function to get tree names by level

---
+++ apps/mobile/lib/features/forest/domain/models/tree_level.dart
@@ -0,0 +1,37 @@
+String _treeName(int level) {
+
+  switch (level) {
+
+    case 1:
+      return "새싹";
+
+    case 2:
+      return "풀";
+
+    case 3:
+      return "묘목";
+
+    case 4:
+      return "어린나무";
+
+    case 5:
+      return "큰나무";
+
+    case 6:
+      return "울창한 나무";
+
+    case 7:
+      return "거목";
+
+    case 8:
+      return "신목";
+
+    case 9:
+      return "숲의 수호목";
+
+    default:
+      return "생명의 나무";
+
+  }
+
+}
