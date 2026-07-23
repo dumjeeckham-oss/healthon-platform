e9841ed99ff1c6d763c3710972252a163bfcf9ca
dumjeeckham-oss <dumjeeckham@gmail.com>
2026-07-23 12:03:02 +0900

    Refactor SoundService to consolidate play methods
    Refactored SoundService to use a generic play method for audio files, reducing code duplication.

--- apps/mobile/lib/core/services/sound_service.dart
+++ apps/mobile/lib/core/services/sound_service.dart
@@ -1,32 +1,38 @@
 import 'package:audioplayers/audioplayers.dart';
 
 class SoundService {
-
   SoundService._();
 
-  static final instance = SoundService._();
+  static final SoundService instance = SoundService._();
 
   final AudioPlayer _player = AudioPlayer();
 
-  Future<void> playLevelUp() async {
+  Future<void> _play(String file) async {
+    await _player.stop();
 
     await _player.play(
-      AssetSource("sounds/level_up.mp3"),
+      AssetSource("sounds/$file"),
+      mode: PlayerMode.lowLatency,
     );
+  }
+
+  Future<void> playLevelUp() async {
+    await _play("level_up.mp3");
   }
 
   Future<void> playBadge() async {
-
-    await _player.play(
-      AssetSource("sounds/badge.mp3"),
-    );
+    await _play("badge.mp3");
   }
 
-  Future<void> playForest() async {
-
-    await _player.play(
-      AssetSource("sounds/forest.mp3"),
-    );
+  Future<void> playForestGrow() async {
+    await _play("forest.mp3");
   }
 
+  Future<void> playClick() async {
+    await _play("click.mp3");
+  }
+
+  Future<void> playSuccess() async {
+    await _play("success.mp3");
+  }
 }
